859f6a98311f6c0e809de4f7dc8fe29381b01702
superman <goodnesskolapo@gmail.com>
2023-02-20 16:55:05 +0100

    started

---
+++ contracts/randomnumber.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract RandNumGen {
+    constructor() {
+        
+    }
+}
